25b3a15bb4cffaa90115616e332c1840e54b6f91
Danilo Amaral <daniloamaralr@gmail.com>
2019-04-12 17:14:00 -0300

    Add Logistic Regression

--- Part 2 - Regression/Section 9 - Random Forest Regression/randomForest.py
+++ Part 2 - Regression/Section 9 - Random Forest Regression/randomForest.py
@@ -1,4 +1,4 @@
-# Decision Tree Regression
+# Random Forest Regression
 
 # Importing the libraries
 import numpy as np
@@ -23,33 +23,22 @@ y = y.reshape(-1,1)
 y = sc_y.fit_transform(y)"""
 
 
-# Fitting Decision Tree Regression to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
-regressor.fit(X, y)
+# Fitting Random Forest Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X,y)
 
 #Predict a new Result
 y_pred = regressor.predict([[6.5]])
-#Because scaling is done wew need to aplly the inverse transform
-#y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
 
-#Without Scaling the y_pred is 130000 and plot the function regressor is a line - apply Scaling is needed
-
-# Visualising the Decision Tree Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
  
 
-# Visualising the Decision Tree results (for higher resolution and smoother curve)
+# Visualising the Random Forest results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
