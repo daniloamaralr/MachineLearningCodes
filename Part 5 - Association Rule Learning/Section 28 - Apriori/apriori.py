84f0e14744b6ad90ded7c6265d8806203d1c91ae
Danilo Amaral <daniloamaralr@gmail.com>
2019-04-24 16:59:57 -0300

    Adjusting results vizualization in Apriori

--- Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -1,20 +1,27 @@
-#Apriori
- 
-# Importing the Libraries
+# Apriori
+
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Import the mall dataset with pandas
-dataset = pd.read_csv('../../datasets/Market_Basket_Optimisation.csv', header=None)
+# Data Preprocessing
+dataset = pd.read_csv('../../datasets/Market_Basket_Optimisation.csv', header = None)
 transactions = []
-for i in range(0,7501):
-    transactions.append([str(dataset.values[i,j])for j in range(0,20)])
-    
-# Training a Apriori on the Dataset
-# Determine the min_support = 3*7 / 7500
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training Apriori on the dataset
 from apyori import apriori
-rules = apriori(transactions,min_support  = 0.003,min_confidence = 0.2,min_lift= 3 , min_length = 2)
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
 # Visualising the results
 results = list(rules)
+
+the_rules = []
+for result in results:
+    the_rules.append({'rule': ','.join(result.items),
+                      'support':result.support,
+                      'confidence':result.ordered_statistics[0].confidence,
+                      'lift':result.ordered_statistics[0].lift})
+df = pd.DataFrame(the_rules, columns = ['rule', 'support', 'confidence', 'lift'])
