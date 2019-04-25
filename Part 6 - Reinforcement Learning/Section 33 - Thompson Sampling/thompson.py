4479e23f36c5ba18e34526601bd55f5ba5b5f469
Danilo Amaral <daniloamaralr@gmail.com>
2019-04-25 17:03:57 -0300

    Add reiforcement Learning - UCB

---
+++ Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/thompson.py
@@ -0,0 +1,9 @@
+#Thompson Sampling
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the mall dataset with pandas
+dataset = pd.read_csv('../../datasets/Ads_CTR_Optimisation.csv')
