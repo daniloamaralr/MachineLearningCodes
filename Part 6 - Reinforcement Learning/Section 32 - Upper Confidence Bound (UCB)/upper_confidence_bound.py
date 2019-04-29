fa4f7cec6ee4ee758ee268461e40d8fec06e2c75
Danilo Amaral <daniloamaralr@gmail.com>
2019-04-29 15:12:43 -0300

    Add Natural Language Processing

--- Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -35,3 +35,10 @@ for n in range(0,N):
     sum_of_rewards[ad] = sum_of_rewards[ad] + reward
     total_reward = total_reward + reward
     
+#Visualising the results
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
+    
